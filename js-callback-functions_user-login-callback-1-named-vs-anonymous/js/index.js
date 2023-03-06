8505dea5858df08d01dcac91445048c5d424a521
DusanRajcevic1989 <dule.rajca@gmail.com>
2023-03-06 10:43:39 +0100

    add js-callback function 1.st challenge

---
+++ js-callback-functions_user-login-callback-1-named-vs-anonymous/js/index.js
@@ -0,0 +1,11 @@
+console.clear();
+
+function handleUserLogin(onSuccess) {
+  // You might wonder why this function is called `onSuccess`. This will become clear by the fourth challenge! 🤞
+  onSuccess();
+}
+function showWelcomeMessage() {
+  console.log("Welcome! You are logged in now.");
+}
+// Call handleUserLogin below!
+handleUserLogin(showWelcomeMessage);
