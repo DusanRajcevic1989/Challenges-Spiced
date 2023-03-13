360656f1c5a476a06c9792eafaff4dcd10b901b0
DusanRajcevic1989 <dule.rajca@gmail.com>
2023-03-13 12:55:51 +0100

    add Get-animal challenge

---
+++ js-unit-testing_get-animal/index.js
@@ -0,0 +1,8 @@
+export function getAnimal(bread) {
+  if (bread === "cats") {
+    return "I totally love cats!";
+  } else if (bread === "dogs") {
+    return "I like dogs!";
+  }
+  return "I do not like animals at all!";
+}
